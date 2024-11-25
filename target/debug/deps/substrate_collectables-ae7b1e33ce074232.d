31b48dad3407ab87984fa5ee698570064658dc51
Yashic <93923672+Yash1c@users.noreply.github.com>
2024-11-25 18:15:50 -0300

    Create my new repository

---
+++ target/debug/deps/substrate_collectables-ae7b1e33ce074232.d
@@ -0,0 +1,5 @@
+C:\Users\Yashic\substrate_collectables\target\debug\deps\libsubstrate_collectables-ae7b1e33ce074232.rmeta: src/main.rs
+
+C:\Users\Yashic\substrate_collectables\target\debug\deps\substrate_collectables-ae7b1e33ce074232.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/substrate_collectables-fb291fac5935840f.d
@@ -0,0 +1,5 @@
+C:\Users\Yashic\substrate_collectables\target\debug\deps\libsubstrate_collectables-fb291fac5935840f.rmeta: src/main.rs
+
+C:\Users\Yashic\substrate_collectables\target\debug\deps\substrate_collectables-fb291fac5935840f.d: src/main.rs
+
+src/main.rs:
